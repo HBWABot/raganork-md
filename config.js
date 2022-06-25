43863f11be2bf292c88ef8612c32807bdebc862e
HBWABot <108124371+HBWABot@users.noreply.github.com>
2022-06-25 10:02:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -19,14 +19,14 @@ module.exports = {
     SESSION: process.env.SESSION || '',
     RG: process.env.RG || '918416093656-1632403322@g.us',
     PASSWORD: process.env.PASSWORD || '',
-    BOT_INFO: process.env.BOT_INFO || 'HBWABot;Souravkl11;918416093656;https://i.imgur.com/tVG2FyU.jpeg;https://chat.whatsapp.com/IgV6un73n0J9TfzyVsaBRA',
+    BOT_INFO: process.env.BOT_INFO || 'HBWABot;Herbert;918416093656;https://i.imgur.com/tVG2FyU.jpeg;https://chat.whatsapp.com/IgV6un73n0J9TfzyVsaBRA',
     RBG_KEY: process.env.RBG_KEY || '',
     ALLOWED: process.env.ALLOWED || '91,94,212',
-    CHATBOT: process.env.CHATBOT || 'on',
+    CHATBOT: process.env.CHATBOT || 'off',
     HANDLERS: process.env.HANDLERS || '.,',
     STICKER_DATA: process.env.STICKER_DATA,
     BOT_NAME: process.env.BOT_NAME || 'HBWABot,
-    AUDIO_DATA: process.env.AUDIO_DATA === undefined || process.env.AUDIO_DATA === "private" ? 'ꪶ͢٭𝑺𝜣𝑼𝑹𝛢𝑽𝑲𝑳¹¹ꫂ;HBWABot;https://www.linkpicture.com/q/rgnk.jpg' : process.env.AUDIO_DATA,
+    AUDIO_DATA: process.env.AUDIO_DATA === undefined || process.env.AUDIO_DATA === "private" ? 'Herbert¹¹ꫂ;HBWABot;https://www.linkpicture.com/q/rgnk.jpg' : process.env.AUDIO_DATA,
     TAKE_KEY: process.env.TAKE_KEY || '',
     MODE: process.env.MODE || 'private',
     WARN: process.env.WARN || '4',
@@ -34,7 +34,7 @@ module.exports = {
     HEROKU: {
         HEROKU: process.env.HEROKU === undefined ? false : convertToBool(process.env.HEROKU),
         API_KEY: process.env.HEROKU_API_KEY || '',
-        APP_NAME: process.env.HEROKU_APP_NAME || 'wabot98'
+        APP_NAME: process.env.HEROKU_APP_NAME || ''
     },
     DATABASE_URL: DATABASE_URL,
     DATABASE: DATABASE_URL === './bot.db' ? new Sequelize({ dialect: "sqlite", storage: DATABASE_URL, logging: DEBUG }) : new Sequelize(DATABASE_URL, { dialectOptions: { ssl: { require: true, rejectUnauthorized: false } }, logging: DEBUG }),
